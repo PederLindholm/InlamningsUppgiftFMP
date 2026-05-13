85772d1d87b5b4b965c67e8e195d7a366c266868
Peder Lindholm <peder.lindholm@hotmail.com>
2026-05-13 10:16:27 +0200

    Initial commit

---
+++ src/main/java/com/example/inlamningsuppgiftfmp/InlamningsUppgiftFmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.inlamningsuppgiftfmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InlamningsUppgiftFmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InlamningsUppgiftFmpApplication.class, args);
+	}
+
+}
