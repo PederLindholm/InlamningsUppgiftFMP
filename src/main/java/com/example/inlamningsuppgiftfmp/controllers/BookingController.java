a2fc7aca1eacffd9de0e65a00ebd5fd40333b4aa
Mai Do <ngocmai.do95@gmail.com>
2026-05-22 13:39:32 +0200

    small fixes

--- src/main/java/com/example/inlamningsuppgiftfmp/controllers/BookingController.java
+++ src/main/java/com/example/inlamningsuppgiftfmp/controllers/BookingController.java
@@ -1,52 +1,31 @@
 package com.example.inlamningsuppgiftfmp.controllers;
 
-import com.example.inlamningsuppgiftfmp.models.Booking;
-import com.example.inlamningsuppgiftfmp.models.Customer;
-import com.example.inlamningsuppgiftfmp.models.Room;
-import com.example.inlamningsuppgiftfmp.repos.BookingRepo;
-import com.example.inlamningsuppgiftfmp.repos.CustomerRepo;
-import com.example.inlamningsuppgiftfmp.repos.RoomRepo;
+import com.example.inlamningsuppgiftfmp.services.BookingService;
+import com.example.inlamningsuppgiftfmp.services.CustomerService;
+import com.example.inlamningsuppgiftfmp.services.RoomService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@RestController
-@RequestMapping("bookings")
+@Controller
+@RequestMapping(path = "/booking")
 public class BookingController {
 
-    private final CustomerRepo customerRepo;
-    private final BookingRepo bookingRepo;
-    private final RoomRepo roomRepo;
+    private final CustomerService customerService;
+    private final BookingService bookingService;
+    private final RoomService roomService;
 
-    public BookingController(CustomerRepo customerRepo, BookingRepo bookingRepo, RoomRepo roomRepo) {
-        this.customerRepo = customerRepo;
-        this.bookingRepo = bookingRepo;
-        this.roomRepo = roomRepo;
+    public BookingController(CustomerService customerService, BookingService bookingService, RoomService roomService) {
+        this.customerService = customerService;
+        this.bookingService = bookingService;
+        this.roomService = roomService;
     }
 
-    @GetMapping
-    public List<Booking> getAllBookings() {
-        return bookingRepo.findAll();
-    }
 
-    @RequestMapping("delete/{id}")
-    public String deleteBooking(@PathVariable Long id) {
-        bookingRepo.deleteById(id);
-        return "Booking " + id + " deleted!";
-    }
 
-    @PostMapping("add")
-    public String addBooking(@RequestParam Long customerId, @RequestParam Long roomId, @RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
-        Customer customer = customerRepo.findById(customerId).orElse(null);
-        if (customer == null) {
-            return "customer does not exist";
-        }
-        Room room = roomRepo.findById(roomId).get();
-        if (customer != null && room != null) {
-            bookingRepo.save(new Booking(customer, room, startDate, endDate));
-            return "Booking added";
-        }
-        return "Invalid ID";
-    }
+
+
 }
--- src/main/java/com/example/inlamningsuppgiftfmp/dtos/BookingDto.java
+++ src/main/java/com/example/inlamningsuppgiftfmp/dtos/BookingDto.java
@@ -2,9 +2,15 @@ package com.example.inlamningsuppgiftfmp.dtos;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingDto {
 
     private Long id;
@@ -21,43 +27,4 @@ public class BookingDto {
     @NotNull
     private LocalDate endDate;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public Long getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(Long roomId) {
-        this.roomId = roomId;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
 }
